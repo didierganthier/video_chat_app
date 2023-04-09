5c86ff950c07bab24656ea6dd0df5d29d6befd57
Didier Peran Ganthier <didierganthierperan@gmail.com>
2023-04-09 19:57:11 -0400

    Add Getx blueprint and state

---
+++ lib/pages/frame/welcome/controller.dart
@@ -0,0 +1,15 @@
+import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:video_chat_app/pages/frame/welcome/state.dart';
+
+class WelcomeController extends GetxController {
+  WelcomeController();
+  final title = "Chatty .";
+  final subtitle = "A new way to chat with your friends.";
+  final state = WelcomeState();
+
+  @override
+  void onReady() {
+    super.onReady();
+    print("WelcomeController onReady");
+  }
+}
